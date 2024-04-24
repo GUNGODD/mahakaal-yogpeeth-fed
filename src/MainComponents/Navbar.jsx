172fb1eebf651c70c1a9e63231033bbe1790186a
GUNGODD <chauhanadityac01@gmail.com>
2024-04-24 17:00:48 +0530

    self created navbar #2

--- src/MainComponents/Navbar.jsx
+++ src/MainComponents/Navbar.jsx
@@ -2,14 +2,29 @@ import React from "react";
 
 export const Navbar = () => {
   return (
-    <div className=" flex  container p-3 m-3 mx-auto bg-emerald-5">
-      <nav className="  flex flex-row  navbar">
-        <div className="logo">
-          <h1> Logo </h1>
-        </div>
-        <p> navbar</p>
+    <div className=" flex flex-row gap-5 m-2 p-3 z-10  mx-auto bg-emerald-5 border broder-black-900">
+      <nav className="flex flex-row gap-5  ">
+        <ul className="flex gap-3 ">
+          <li>
+            <a href="#Home"> Home </a>
+          </li>
+          <li>
+            <a href="#Couses">Courses </a>
+          </li>
+          <li>
+            <a href="#Teacehrs "> Our Teachers </a>
+          </li>
+          <li>
+            <a href="#Gallery "> Gallery </a>
+          </li>
+        </ul>
       </nav>
-      <p> container</p>
+      <div className="flex m-auto ">
+        <h2>Logo </h2>
+      </div>
+      <div>
+        <span>contact us </span>
+      </div>
     </div>
   );
 };
