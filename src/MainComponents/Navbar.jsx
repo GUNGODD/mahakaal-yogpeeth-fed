05cc2c910025ad47481dc35db2ea1a8b18f1d042
GUNGODD <chauhanadityac01@gmail.com>
2024-04-24 16:25:48 +0530

    self created nav progress

---
+++ src/MainComponents/Navbar.jsx
@@ -0,0 +1,15 @@
+import React from "react";
+
+export const Navbar = () => {
+  return (
+    <div className=" flex  container p-3 m-3 mx-auto bg-emerald-5">
+      <nav className="  flex flex-row  navbar">
+        <div className="logo">
+          <h1> Logo </h1>
+        </div>
+        <p> navbar</p>
+      </nav>
+      <p> container</p>
+    </div>
+  );
+};
