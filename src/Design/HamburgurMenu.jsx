9b14c3b03177d19e6d33f759c745e116b9010e5c
GUNGODD <chauhanadityac01@gmail.com>
2024-05-12 21:14:36 +0530

    navbar hambg fixed

--- src/Design/HamburgurMenu.jsx
+++ src/Design/HamburgurMenu.jsx
@@ -1,9 +1,9 @@
 import React, { useState } from "react";
 import { MotionConfig, motion } from "framer-motion";
 
-export const HamMenue = () => {
+export const Example = () => {
   return (
-    <div className="grid h-screen place-content-center bg-gradient-to-br from-violet-500 to-indigo-500">
+    <div className="grid h-1.5  w-3  place-content-center bg-gradient-to-br from-violet-500 to-indigo-500">
       <AnimatedHamburgerButton />
     </div>
   );
@@ -22,21 +22,21 @@ const AnimatedHamburgerButton = () => {
         initial={false}
         animate={active ? "open" : "closed"}
         onClick={() => setActive((pv) => !pv)}
-        className="relative h-10 w-10 rounded-full bg-white/0 transition-colors hover:bg-white/20"
+        className="relative h-10 w-3/6 rounded-full bg-white/0 transition-colors hover:bg-white/20"
       >
         <motion.span
           variants={VARIANTS.top}
-          className="absolute h-1 w-5 bg-white"
+          className="absolute h-1 w-10 bg-white"
           style={{ y: "-50%", left: "50%", x: "-50%", top: "35%" }}
         />
         <motion.span
           variants={VARIANTS.middle}
-          className="absolute h-1 w-5 bg-white"
+          className="absolute h-1 w-10 bg-white"
           style={{ left: "50%", x: "-50%", top: "50%", y: "-50%" }}
         />
         <motion.span
           variants={VARIANTS.bottom}
-          className="absolute h-1 w-8 bg-white"
+          className="absolute h-1 w-3 bg-white"
           style={{
             x: "-50%",
             y: "50%",
