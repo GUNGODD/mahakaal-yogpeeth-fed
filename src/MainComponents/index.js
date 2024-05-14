84bab7b101b3be4cdb61db873b60a540b99c49d3
anuragnegi000 <anurag.negi862@gmail.com>
2024-05-14 19:07:57 +0530

    fixes?

--- src/MainComponents/index.js
+++ src/MainComponents/index.js
@@ -1,14 +1,14 @@
-import teacher1 from "../assets/glimpse/teachers/img1.jpg";
-import teacher2 from "../assets/glimpse/teachers/img2.jpg";
-import teacher3 from "../assets/glimpse/teachers/img3.jpg";
-import teacher4 from "../assets/glimpse/teachers/img4.jpg";
-import teacher5 from "../assets/glimpse/teachers/img5.jpg";
+import img1 from "../assets/glimpse/teachers/img1.jpg";
+import img2 from "../assets/glimpse/teachers/img2.jpg";
+import img3 from "../assets/glimpse/teachers/img3.jpg";
+import img4 from "../assets/glimpse/teachers/img4.jpg";
+import img5 from "../assets/glimpse/teachers/img5.jpg";
 
 export default{
-    teacher1,
-    teacher2,
-    teacher3,
-    teacher4,
-    teacher5,   
+    img1,
+    img2,
+    img3,
+    img4,
+    img5,   
 }
 
