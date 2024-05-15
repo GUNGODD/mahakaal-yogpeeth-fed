97ea7f9494f9038988ba85284694e51341c7fd83
anuragnegi000 <anurag.negi862@gmail.com>
2024-05-15 20:53:39 +0530

    yoga ttc section

---
+++ src/Pages/Parts/ImgContent.jsx
@@ -0,0 +1,31 @@
+import React from 'react'
+
+const ImgContent = () => {
+  return (
+    <div id="imgContent">
+        <img src="https://plus.unsplash.com/premium_photo-1669144690665-17dde1269f68?q=80&w=2069&auto=format&fit=crop&ixlib=rb-4.0.3&ixid=M3wxMjA3fDB8MHxwaG90by1wYWdlfHx8fGVufDB8fHx8fA%3D%3D" alt="" />
+       <div id="textArea">
+        <h1>Lorem ipsum dolor sit.</h1>
+        <h4>Lorem ipsum, dolor sit amet consectetur adipisicing elit. Voluptates fuga, quam temporibus, quo magnam fugit at id ex repellendus quos nulla? Quia assumenda dolores minus maiores accusamus odio eius similique doloribus nulla ad quos, exercitationem, perspiciatis deserunt. Unde voluptate libero repudiandae quis! Sed voluptatum, quas nostrum enim quod aliquam necessitatibus.</h4>
+        <button>Enroll now!!</button>
+       </div>
+    </div>
+  )
+}
+
+export const ImgContentMid = () => {
+    return (
+      <div id="imgContent">
+          
+         <div id="textArea">
+          <h1>Lorem ipsum dolor sit.</h1>
+          <h4>Lorem ipsum, dolor sit amet consectetur adipisicing elit. Voluptates fuga, quam temporibus, quo magnam fugit at id ex repellendus quos nulla? Quia assumenda dolores minus maiores accusamus odio eius similique doloribus nulla ad quos, exercitationem, perspiciatis deserunt. Unde voluptate libero repudiandae quis! Sed voluptatum, quas nostrum enim quod aliquam necessitatibus.</h4>
+          <button>Enroll now!!</button>
+          
+         </div>
+         <img src="https://plus.unsplash.com/premium_photo-1669144690665-17dde1269f68?q=80&w=2069&auto=format&fit=crop&ixlib=rb-4.0.3&ixid=M3wxMjA3fDB8MHxwaG90by1wYWdlfHx8fGVufDB8fHx8fA%3D%3D" alt="" />
+      </div>
+    )
+  }
+
+export default ImgContent
