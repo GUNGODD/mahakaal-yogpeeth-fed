ec0f75c416b7b286847ddf34375c3563b92298d8
anuragnegi000 <anurag.negi862@gmail.com>
2024-05-15 19:34:13 +0530

    changes in color

--- src/Design/SuffleHero.jsx
+++ src/Design/SuffleHero.jsx
@@ -6,7 +6,7 @@ const ShuffleHero = () => {
   return (
     <section className="w-full px-8 py-12 grid grid-cols-1 md:grid-cols-2 items-center gap-8 max-w-6xl mx-auto">
       <div>
-        <span className="block mb-4 text-xs md:text-sm text-indigo-500 font-medium">
+        <span className="block mb-4 text-xs md:text-sm text-green-600 font-medium">
           Better every day
         </span>
         <h3 className="text-4xl md:text-6xl font-semibold">
@@ -16,8 +16,8 @@ const ShuffleHero = () => {
           Lorem ipsum dolor sit amet consectetur, adipisicing elit. Nam nobis in
           error repellat voluptatibus Sad.
         </p>
-        <button className="bg-indigo-500 text-white font-medium py-2 px-4 rounded transition-all hover:bg-indigo-600 active:scale-95">
-          Find a class
+        <button className="bg-green-600 text-white font-medium py-2 px-4 rounded transition-all hover:bg-green-700 active:scale-65">
+          Enroll now!!
         </button>
       </div>
       <ShuffleGrid />
