4d5fd7af741d8474cfe3232486e2c5923aee707c
GUNGODD <chauhanadityac01@gmail.com>
2024-05-25 00:42:57 +0530

    About us Page

---
+++ src/Components/AboutUs/MainAbout.jsx
@@ -0,0 +1,24 @@
+import Footer from "../../MainComponents/Footer"
+import Navbar from "../../MainComponents/Navbar"
+import GridGallery from "./GridGallery"
+
+
+export const MainAbout = () => {
+  return (
+
+
+    <>
+      <div>
+
+
+        <Navbar />
+
+        <GridGallery />
+        <Footer />
+
+      </div>
+
+    </>
+
+  )
+}
--- src/Components/Gallery/MainGallery.jsx
+++ src/Components/Gallery/MainGallery.jsx
@@ -1,22 +1,11 @@
 import Footer from "../../MainComponents/Footer";
 import Navbar from "../../MainComponents/Navbar";
-import ImgScroll from "../../Pages/Parts/ImgScroll";
-import AiCard from "./AiCard";
-import GridGallery from "./GridGallery";
-
-import HeroSlider from "./HeroSlider";
 
 
 export default function MainGallery() {
   return (
     <>
       <Navbar />
-
-      {/* <HeroSlider /> */}
-<AiCard/>
-<ImgScroll/>
-      {/* <GridGallery /> */}
-      
 
 
       <Footer />
