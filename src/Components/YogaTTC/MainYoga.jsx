e341caef971dcbd4079cfc458ee10cc874f96fe3
anuragnegi000 <anurag.negi862@gmail.com>
2024-05-25 23:57:04 +0530

    added Hero Section

--- src/Components/YogaTTC/MainYoga.jsx
+++ src/Components/YogaTTC/MainYoga.jsx
@@ -3,11 +3,13 @@ import Navbar from '../../MainComponents/Navbar'
 import PriceGrid from '../../Pages/Parts/PriceGrid'
 import Footer from '../../MainComponents/Footer'
 import LogoHeading from '../../Pages/LogoHeading'
+import { Hero } from '../../Pages/Parts/Hero'
 
 const MainYoga = () => {
   return (
     <div>
         <Navbar/>
+        <Hero/>
         <div className='mt-28'>
         <LogoHeading heading="Upcoming classes"/>
         </div>
