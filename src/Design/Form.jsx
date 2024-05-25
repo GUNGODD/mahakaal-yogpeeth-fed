4deecbb1f41d50ba513ec425db159994c425ffc0
anuragnegi000 <anurag.negi862@gmail.com>
2024-05-25 13:44:42 +0530

    added typewriter effect

--- src/Design/Form.jsx
+++ src/Design/Form.jsx
@@ -1,4 +1,5 @@
 import React from 'react';
+import TypewriterComponent from 'typewriter-effect';
 
 const Form = () => {
   return (
@@ -13,7 +14,15 @@ const Form = () => {
       <section className="relative flex flex-wrap lg:h-screen lg:items-center">
         <div className="w-full px-4 py-12 sm:px-6 sm:py-16 lg:w-1/2 lg:px-8 lg:py-24">
           <div className="mx-auto max-w-lg text-center">
-            <h1 className="text-2xl font-bold sm:text-3xl">Get started today!</h1>
+            <h1 className="text-2xl font-bold sm:text-3xl">
+            <TypewriterComponent
+                options={{
+                  strings: ['Get Started Today !'],
+                  autoStart: true,
+                  loop: true,
+                }}
+              />
+            </h1>
 
             <p className="mt-4 text-gray-500">
               Lorem ipsum dolor sit amet consectetur adipisicing elit. Et libero nulla eaque error neque
