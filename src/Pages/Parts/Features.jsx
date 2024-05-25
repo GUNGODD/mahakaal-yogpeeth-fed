2a01521e059c6f6f0be9e7bb9b61c6ba3a6dfe51
anuragnegi000 <anurag.negi862@gmail.com>
2024-05-25 17:06:06 +0530

    changes in ContentArea (YogaTTC Page)

--- src/Pages/Parts/Features.jsx
+++ src/Pages/Parts/Features.jsx
@@ -10,123 +10,305 @@ const features = [
   export default function Features() {
     return (
       <>
-      {/*
-  Heads up! 👋
+     <section class="bg-gray-900 text-white">
+  <div class="mx-auto max-w-screen-xl px-4 py-8 sm:px-6 sm:py-12 lg:px-8 lg:py-16">
+    <div class="mx-auto max-w-lg text-center">
+      <h2 class="text-3xl font-bold sm:text-4xl">Kickstart your marketing</h2>
 
-  This component comes with some `rtl` classes. Please remove them if they are not needed in your project.
-*/}
+      <p class="mt-4 text-gray-300">
+        Lorem ipsum, dolor sit amet consectetur adipisicing elit. Consequuntur aliquam doloribus
+        nesciunt eos fugiat. Vitae aperiam fugit consequuntur saepe laborum.
+      </p>
+    </div>
 
-<section>
-  <div className="md:ml-20 max-w-screen-xl px-4 py-8 sm:px-6 sm:py-12 lg:px-8 lg:py-16">
-    <div className="grid grid-cols-1 gap-y-8 lg:grid-cols-2 lg:items-center lg:gap-x-16">
-      <div className="mx-auto max-w-lg text-center lg:mx-0 ltr:lg:text-left rtl:lg:text-right">
-        <h2 className="text-3xl font-bold sm:text-4xl">Yoga Teacher Training Classes</h2>
+    <div class="mt-8 grid grid-cols-1 gap-8 md:grid-cols-2 lg:grid-cols-3">
+      <a
+        class="block rounded-xl border border-gray-800 p-8 shadow-xl transition hover:border-pink-500/10 hover:shadow-pink-500/10"
+        href="#"
+      >
+        <svg
+          xmlns="http://www.w3.org/2000/svg"
+          class="size-10 text-pink-500"
+          fill="none"
+          viewBox="0 0 24 24"
+          stroke="currentColor"
+        >
+          <path d="M12 14l9-5-9-5-9 5 9 5z" />
+          <path
+            d="M12 14l6.16-3.422a12.083 12.083 0 01.665 6.479A11.952 11.952 0 0012 20.055a11.952 11.952 0 00-6.824-2.998 12.078 12.078 0 01.665-6.479L12 14z"
+          />
+          <path
+            stroke-linecap="round"
+            stroke-linejoin="round"
+            stroke-width="2"
+            d="M12 14l9-5-9-5-9 5 9 5zm0 0l6.16-3.422a12.083 12.083 0 01.665 6.479A11.952 11.952 0 0012 20.055a11.952 11.952 0 00-6.824-2.998 12.078 12.078 0 01.665-6.479L12 14zm-4 6v-7.5l4-2.222"
+          />
+        </svg>
 
-        <p className="mt-4 text-gray-600">
-          Lorem ipsum dolor sit amet consectetur adipisicing elit. Aut vero aliquid sint distinctio
-          iure ipsum cupiditate? Quis, odit assumenda? Deleniti quasi inventore, libero reiciendis
-          minima aliquid tempora. Obcaecati, autem.
+        <h2 class="mt-4 text-xl font-bold text-white">Digital campaigns</h2>
+
+        <p class="mt-1 text-sm text-gray-300">
+          Lorem ipsum dolor sit amet consectetur adipisicing elit. Ex ut quo possimus adipisci
+          distinctio alias voluptatum blanditiis laudantium.
         </p>
+      </a>
 
-        <a
-          href="/enroll"
-          className="mt-8 inline-block rounded bg-indigo-600 px-12 py-3 text-sm font-medium text-white transition hover:bg-indigo-700 focus:outline-none focus:ring focus:ring-indigo-400"
+      <a
+        class="block rounded-xl border border-gray-800 p-8 shadow-xl transition hover:border-pink-500/10 hover:shadow-pink-500/10"
+        href="#"
+      >
+        <svg
+          xmlns="http://www.w3.org/2000/svg"
+          class="size-10 text-pink-500"
+          fill="none"
+          viewBox="0 0 24 24"
+          stroke="currentColor"
         >
-          Get Started Today
-        </a>
-      </div>
+          <path d="M12 14l9-5-9-5-9 5 9 5z" />
+          <path
+            d="M12 14l6.16-3.422a12.083 12.083 0 01.665 6.479A11.952 11.952 0 0012 20.055a11.952 11.952 0 00-6.824-2.998 12.078 12.078 0 01.665-6.479L12 14z"
+          />
+          <path
+            stroke-linecap="round"
+            stroke-linejoin="round"
+            stroke-width="2"
+            d="M12 14l9-5-9-5-9 5 9 5zm0 0l6.16-3.422a12.083 12.083 0 01.665 6.479A11.952 11.952 0 0012 20.055a11.952 11.952 0 00-6.824-2.998 12.078 12.078 0 01.665-6.479L12 14zm-4 6v-7.5l4-2.222"
+          />
+        </svg>
 
-      <div className="grid grid-cols-2 gap-4 sm:grid-cols-3">
-        <a
-          className="block rounded-xl border border-gray-100 p-4 shadow-sm hover:border-indigo-500 hover:ring-1 hover:ring-indigo-200 focus:outline-none focus:ring"
-          href="#"
+        <h2 class="mt-4 text-xl font-bold text-white">Digital campaigns</h2>
+
+        <p class="mt-1 text-sm text-gray-300">
+          Lorem ipsum dolor sit amet consectetur adipisicing elit. Ex ut quo possimus adipisci
+          distinctio alias voluptatum blanditiis laudantium.
+        </p>
+      </a>
+
+      <a
+        class="block rounded-xl border border-gray-800 p-8 shadow-xl transition hover:border-pink-500/10 hover:shadow-pink-500/10"
+        href="#"
+      >
+        <svg
+          xmlns="http://www.w3.org/2000/svg"
+          class="size-10 text-pink-500"
+          fill="none"
+          viewBox="0 0 24 24"
+          stroke="currentColor"
         >
-          <span className="inline-block rounded-lg bg-gray-50 p-3">
-          <img height={40} width={50} src="https://i.ibb.co/JKngSW4/Mahakaal-Logo2.png" alt="" />
-          </span>
+          <path d="M12 14l9-5-9-5-9 5 9 5z" />
+          <path
+            d="M12 14l6.16-3.422a12.083 12.083 0 01.665 6.479A11.952 11.952 0 0012 20.055a11.952 11.952 0 00-6.824-2.998 12.078 12.078 0 01.665-6.479L12 14z"
+          />
+          <path
+            stroke-linecap="round"
+            stroke-linejoin="round"
+            stroke-width="2"
+            d="M12 14l9-5-9-5-9 5 9 5zm0 0l6.16-3.422a12.083 12.083 0 01.665 6.479A11.952 11.952 0 0012 20.055a11.952 11.952 0 00-6.824-2.998 12.078 12.078 0 01.665-6.479L12 14zm-4 6v-7.5l4-2.222"
+          />
+        </svg>
 
-          <h2 className="mt-2 font-bold">Hatha Yoga</h2>
+        <h2 class="mt-4 text-xl font-bold text-white">Digital campaigns</h2>
 
-          <p className="hidden sm:mt-1 sm:block sm:text-sm sm:text-gray-600">
-            Lorem ipsum dolor sit amet consectetur.
-          </p>
-        </a>
+        <p class="mt-1 text-sm text-gray-300">
+          Lorem ipsum dolor sit amet consectetur adipisicing elit. Ex ut quo possimus adipisci
+          distinctio alias voluptatum blanditiis laudantium.
+        </p>
+      </a>
 
-        <a
-          className="block rounded-xl border border-gray-100 p-4 shadow-sm hover:border-indigo-500 hover:ring-1 hover:ring-indigo-200 focus:outline-none focus:ring"
-          href="#"
+      <a
+        class="block rounded-xl border border-gray-800 p-8 shadow-xl transition hover:border-pink-500/10 hover:shadow-pink-500/10"
+        href="#"
+      >
+        <svg
+          xmlns="http://www.w3.org/2000/svg"
+          class="size-10 text-pink-500"
+          fill="none"
+          viewBox="0 0 24 24"
+          stroke="currentColor"
         >
-          <span className="inline-block rounded-lg bg-gray-50 p-3">
-          <img height={40} width={50} src="https://i.ibb.co/JKngSW4/Mahakaal-Logo2.png" alt="" />
-          </span>
+          <path d="M12 14l9-5-9-5-9 5 9 5z" />
+          <path
+            d="M12 14l6.16-3.422a12.083 12.083 0 01.665 6.479A11.952 11.952 0 0012 20.055a11.952 11.952 0 00-6.824-2.998 12.078 12.078 0 01.665-6.479L12 14z"
+          />
+          <path
+            stroke-linecap="round"
+            stroke-linejoin="round"
+            stroke-width="2"
+            d="M12 14l9-5-9-5-9 5 9 5zm0 0l6.16-3.422a12.083 12.083 0 01.665 6.479A11.952 11.952 0 0012 20.055a11.952 11.952 0 00-6.824-2.998 12.078 12.078 0 01.665-6.479L12 14zm-4 6v-7.5l4-2.222"
+          />
+        </svg>
 
-          <h2 className="mt-2 font-bold">Ashtanga Yoga</h2>
+        <h2 class="mt-4 text-xl font-bold text-white">Digital campaigns</h2>
 
-          <p className="hidden sm:mt-1 sm:block sm:text-sm sm:text-gray-600">
-            Lorem ipsum dolor sit amet consectetur.
-          </p>
-        </a>
+        <p class="mt-1 text-sm text-gray-300">
+          Lorem ipsum dolor sit amet consectetur adipisicing elit. Ex ut quo possimus adipisci
+          distinctio alias voluptatum blanditiis laudantium.
+        </p>
+      </a>
 
-        <a
-          className="block rounded-xl border border-gray-100 p-4 shadow-sm hover:border-indigo-500 hover:ring-1 hover:ring-indigo-200 focus:outline-none focus:ring"
-          href="#"
+      <a
+        class="block rounded-xl border border-gray-800 p-8 shadow-xl transition hover:border-pink-500/10 hover:shadow-pink-500/10"
+        href="#"
+      >
+        <svg
+          xmlns="http://www.w3.org/2000/svg"
+          class="size-10 text-pink-500"
+          fill="none"
+          viewBox="0 0 24 24"
+          stroke="currentColor"
         >
-          <span className="inline-block rounded-lg bg-gray-50 p-3">
-            <img height={40} width={50} src="https://i.ibb.co/JKngSW4/Mahakaal-Logo2.png" alt="" />
-          </span>
+          <path d="M12 14l9-5-9-5-9 5 9 5z" />
+          <path
+            d="M12 14l6.16-3.422a12.083 12.083 0 01.665 6.479A11.952 11.952 0 0012 20.055a11.952 11.952 0 00-6.824-2.998 12.078 12.078 0 01.665-6.479L12 14z"
+          />
+          <path
+            stroke-linecap="round"
+            stroke-linejoin="round"
+            stroke-width="2"
+            d="M12 14l9-5-9-5-9 5 9 5zm0 0l6.16-3.422a12.083 12.083 0 01.665 6.479A11.952 11.952 0 0012 20.055a11.952 11.952 0 00-6.824-2.998 12.078 12.078 0 01.665-6.479L12 14zm-4 6v-7.5l4-2.222"
+          />
+        </svg>
 
-          <h2 className="mt-2 font-bold">Yoga Anatomy</h2>
+        <h2 class="mt-4 text-xl font-bold text-white">Digital campaigns</h2>
 
-          <p className="hidden sm:mt-1 sm:block sm:text-sm sm:text-gray-600">
-            Lorem ipsum dolor sit amet consectetur.
-          </p>
-        </a>
+        <p class="mt-1 text-sm text-gray-300">
+          Lorem ipsum dolor sit amet consectetur adipisicing elit. Ex ut quo possimus adipisci
+          distinctio alias voluptatum blanditiis laudantium.
+        </p>
+      </a>
 
-        <a
-          className="block rounded-xl border border-gray-100 p-4 shadow-sm hover:border-indigo-500 hover:ring-1 hover:ring-indigo-200 focus:outline-none focus:ring"
-          href="#"
+      <a
+        class="block rounded-xl border border-gray-800 p-8 shadow-xl transition hover:border-pink-500/10 hover:shadow-pink-500/10"
+        href="#"
+      >
+        <svg
+          xmlns="http://www.w3.org/2000/svg"
+          class="size-10 text-pink-500"
+          fill="none"
+          viewBox="0 0 24 24"
+          stroke="currentColor"
         >
-          <span className="inline-block rounded-lg bg-gray-50 p-3">
-          <img height={40} width={50} src="https://i.ibb.co/JKngSW4/Mahakaal-Logo2.png" alt="" />
-          </span>
+          <path d="M12 14l9-5-9-5-9 5 9 5z" />
+          <path
+            d="M12 14l6.16-3.422a12.083 12.083 0 01.665 6.479A11.952 11.952 0 0012 20.055a11.952 11.952 0 00-6.824-2.998 12.078 12.078 0 01.665-6.479L12 14z"
+          />
+          <path
+            stroke-linecap="round"
+            stroke-linejoin="round"
+            stroke-width="2"
+            d="M12 14l9-5-9-5-9 5 9 5zm0 0l6.16-3.422a12.083 12.083 0 01.665 6.479A11.952 11.952 0 0012 20.055a11.952 11.952 0 00-6.824-2.998 12.078 12.078 0 01.665-6.479L12 14zm-4 6v-7.5l4-2.222"
+          />
+        </svg>
 
-          <h2 className="mt-2 font-bold">Yoga Philosophy</h2>
+        <h2 class="mt-4 text-xl font-bold text-white">Digital campaigns</h2>
 
-          <p className="hidden sm:mt-1 sm:block sm:text-sm sm:text-gray-600">
-            Lorem ipsum dolor sit amet consectetur.
-          </p>
-        </a>
+        <p class="mt-1 text-sm text-gray-300">
+          Lorem ipsum dolor sit amet consectetur adipisicing elit. Ex ut quo possimus adipisci
+          distinctio alias voluptatum blanditiis laudantium.
+        </p>
+      </a>
 
-        <a
-          className="block rounded-xl border border-gray-100 p-4 shadow-sm hover:border-indigo-500 hover:ring-1 hover:ring-indigo-200 focus:outline-none focus:ring"
-          href="#"
+      <a
+        class="block rounded-xl border border-gray-800 p-8 shadow-xl transition hover:border-pink-500/10 hover:shadow-pink-500/10"
+        href="#"
+      >
+        <svg
+          xmlns="http://www.w3.org/2000/svg"
+          class="size-10 text-pink-500"
+          fill="none"
+          viewBox="0 0 24 24"
+          stroke="currentColor"
         >
-          <span className="inline-block rounded-lg bg-gray-50 p-3">
-          <img height={40} width={50} src="https://i.ibb.co/JKngSW4/Mahakaal-Logo2.png" alt="" />
-          </span>
+          <path d="M12 14l9-5-9-5-9 5 9 5z" />
+          <path
+            d="M12 14l6.16-3.422a12.083 12.083 0 01.665 6.479A11.952 11.952 0 0012 20.055a11.952 11.952 0 00-6.824-2.998 12.078 12.078 0 01.665-6.479L12 14z"
+          />
+          <path
+            stroke-linecap="round"
+            stroke-linejoin="round"
+            stroke-width="2"
+            d="M12 14l9-5-9-5-9 5 9 5zm0 0l6.16-3.422a12.083 12.083 0 01.665 6.479A11.952 11.952 0 0012 20.055a11.952 11.952 0 00-6.824-2.998 12.078 12.078 0 01.665-6.479L12 14zm-4 6v-7.5l4-2.222"
+          />
+        </svg>
 
-          <h2 className="mt-2 font-bold">Pranayama</h2>
+        <h2 class="mt-4 text-xl font-bold text-white">Digital campaigns</h2>
 
-          <p className="hidden sm:mt-1 sm:block sm:text-sm sm:text-gray-600">
-            Lorem ipsum dolor sit amet consectetur.
-          </p>
-        </a>
+        <p class="mt-1 text-sm text-gray-300">
+          Lorem ipsum dolor sit amet consectetur adipisicing elit. Ex ut quo possimus adipisci
+          distinctio alias voluptatum blanditiis laudantium.
+        </p>
+      </a>
 
-        <a
-          className="block rounded-xl border border-gray-100 p-4 shadow-sm hover:border-indigo-500 hover:ring-1 hover:ring-indigo-200 focus:outline-none focus:ring"
-          href="#"
+      <a
+        class="block rounded-xl border border-gray-800 p-8 shadow-xl transition hover:border-pink-500/10 hover:shadow-pink-500/10"
+        href="#"
+      >
+        <svg
+          xmlns="http://www.w3.org/2000/svg"
+          class="size-10 text-pink-500"
+          fill="none"
+          viewBox="0 0 24 24"
+          stroke="currentColor"
         >
-          <span className="inline-block rounded-lg bg-gray-50 p-3">
-          <img height={40} width={50} src="https://i.ibb.co/JKngSW4/Mahakaal-Logo2.png" alt="" />
-          </span>
+          <path d="M12 14l9-5-9-5-9 5 9 5z" />
+          <path
+            d="M12 14l6.16-3.422a12.083 12.083 0 01.665 6.479A11.952 11.952 0 0012 20.055a11.952 11.952 0 00-6.824-2.998 12.078 12.078 0 01.665-6.479L12 14z"
+          />
+          <path
+            stroke-linecap="round"
+            stroke-linejoin="round"
+            stroke-width="2"
+            d="M12 14l9-5-9-5-9 5 9 5zm0 0l6.16-3.422a12.083 12.083 0 01.665 6.479A11.952 11.952 0 0012 20.055a11.952 11.952 0 00-6.824-2.998 12.078 12.078 0 01.665-6.479L12 14zm-4 6v-7.5l4-2.222"
+          />
+        </svg>
 
-          <h2 className="mt-2 font-bold">Meditation</h2>
+        <h2 class="mt-4 text-xl font-bold text-white">Digital campaigns</h2>
 
-          <p className="hidden sm:mt-1 sm:block sm:text-sm sm:text-gray-600">
-            Lorem ipsum dolor sit amet consectetur.
-          </p>
-        </a>
-      </div>
+        <p class="mt-1 text-sm text-gray-300">
+          Lorem ipsum dolor sit amet consectetur adipisicing elit. Ex ut quo possimus adipisci
+          distinctio alias voluptatum blanditiis laudantium.
+        </p>
+      </a>
+
+      <a
+        class="block rounded-xl border border-gray-800 p-8 shadow-xl transition hover:border-pink-500/10 hover:shadow-pink-500/10"
+        href="#"
+      >
+        <svg
+          xmlns="http://www.w3.org/2000/svg"
+          class="size-10 text-pink-500"
+          fill="none"
+          viewBox="0 0 24 24"
+          stroke="currentColor"
+        >
+          <path d="M12 14l9-5-9-5-9 5 9 5z" />
+          <path
+            d="M12 14l6.16-3.422a12.083 12.083 0 01.665 6.479A11.952 11.952 0 0012 20.055a11.952 11.952 0 00-6.824-2.998 12.078 12.078 0 01.665-6.479L12 14z"
+          />
+          <path
+            stroke-linecap="round"
+            stroke-linejoin="round"
+            stroke-width="2"
+            d="M12 14l9-5-9-5-9 5 9 5zm0 0l6.16-3.422a12.083 12.083 0 01.665 6.479A11.952 11.952 0 0012 20.055a11.952 11.952 0 00-6.824-2.998 12.078 12.078 0 01.665-6.479L12 14zm-4 6v-7.5l4-2.222"
+          />
+        </svg>
+
+        <h2 class="mt-4 text-xl font-bold text-white">Digital campaigns</h2>
+
+        <p class="mt-1 text-sm text-gray-300">
+          Lorem ipsum dolor sit amet consectetur adipisicing elit. Ex ut quo possimus adipisci
+          distinctio alias voluptatum blanditiis laudantium.
+        </p>
+      </a>
+    </div>
+
+    <div class="mt-12 text-center">
+      <a
+        href="#"
+        class="inline-block rounded bg-pink-600 px-12 py-3 text-sm font-medium text-white transition hover:bg-pink-700 focus:outline-none focus:ring focus:ring-yellow-400"
+      >
+        Get Started Today
+      </a>
     </div>
   </div>
 </section>
