7967015d055f8a6e2e4e85a471c5c0c241947b17
anuragnegi000 <anurag.negi862@gmail.com>
2024-05-27 20:54:13 +0530

    basic changes in retreat section

--- src/Components/YogaRetreat10/YogaRetreat10.jsx
+++ src/Components/YogaRetreat10/YogaRetreat10.jsx
@@ -3,12 +3,14 @@ import Navbar from '../../MainComponents/Navbar'
 import AiCard from '../Gallery/AiCard'
 import Footer from '../../MainComponents/Footer'
 import { HeroImage } from '../AboutUs/MainAbout'
+import LogoHeading from '../../Pages/LogoHeading'
 
 const YogaRetreat10 = () => {
   return (
     <div>
         <Navbar/>
         <HeroImage wallpaper={"https://upload.wikimedia.org/wikipedia/commons/7/75/Rishikesh-Lakshman_Jhula_by_Kaustubh_Nayyar.jpg"} Heading={"This is Heading"}/>
+        <LogoHeading heading={"10 Days Yoga Retreat in Rishikesh Overview"}/>
         <Footer/>
     </div>
   )
--- src/Components/YogaRetreat3/YogaRetreat3.jsx
+++ src/Components/YogaRetreat3/YogaRetreat3.jsx
@@ -3,12 +3,14 @@ import Navbar from '../../MainComponents/Navbar'
 import AiCard from '../Gallery/AiCard'
 import Footer from '../../MainComponents/Footer'
 import { HeroImage } from '../AboutUs/MainAbout'
+import LogoHeading from '../../Pages/LogoHeading'
 
 const YogaRetreat3 = () => {
   return (
     <div>
         <Navbar/>
         <HeroImage wallpaper={"https://upload.wikimedia.org/wikipedia/commons/7/75/Rishikesh-Lakshman_Jhula_by_Kaustubh_Nayyar.jpg"} Heading={"This is Heading"}/>
+        <LogoHeading heading={"3 Days Yoga Retreat in Rishikesh Overview"}/>
         <Footer/>
     </div>
   ) 
