7736614a6736d7c999ed489a1ea85ee50b77808b
anuragnegi000 <anurag.negi862@gmail.com>
2024-05-29 00:31:57 +0530

    changes in retreat

--- src/Components/YogaRetreat3/YogaRetreat3.jsx
+++ src/Components/YogaRetreat3/YogaRetreat3.jsx
@@ -12,13 +12,276 @@ const YogaRetreat3 = () => {
     <div>
         <Navbar/>
         <HeroImage wallpaper={"https://upload.wikimedia.org/wikipedia/commons/7/75/Rishikesh-Lakshman_Jhula_by_Kaustubh_Nayyar.jpg"} Heading={"This is Heading"}/>
+        <div className='mt-10 mb-10'>
         <LogoHeading heading={"3 Days Yoga Retreat in Rishikesh Overview"}/>
+        </div>
+        <RetreatForm/>
         <TypewriterHeading/>
+        <FAQSection/>
         <OnePagerSection/>
         <Footer/>
     </div>
   ) 
 }
+
+const RetreatForm = ()=>{
+  return (
+    
+<section className="bg-white">
+  <div className="lg:grid lg:grid-cols-12">
+    <section className="relative flex h-32 items-end rounded-lg bg-gray-900 lg:col-span-5 lg:h-full xl:col-span-6 ">
+      <img
+        alt=""
+        src="https://img.etimg.com/thumb/msid-77107804,width-640,height-480,imgsize-574047,resizemode-4/pictures-shared.jpg"
+        className="absolute inset-0 h-full w-full object-cover opacity-80"
+      />
+
+      <div className="hidden lg:relative lg:block lg:p-12">
+        <a className="block text-white" href="#">
+          <span className="sr-only">Home</span>
+          <svg
+            className="h-8 sm:h-10"
+            viewBox="0 0 28 24"
+            fill="none"
+            xmlns="http://www.w3.org/2000/svg"
+          >
+            <path
+              d="M0.41 10.3847C1.14777 7.4194 2.85643 4.7861 5.2639 2.90424C7.6714 1.02234 10.6393 0 13.695 0C16.7507 0 19.7186 1.02234 22.1261 2.90424C24.5336 4.7861 26.2422 7.4194 26.98 10.3847H25.78C23.7557 10.3549 21.7729 10.9599 20.11 12.1147C20.014 12.1842 19.9138 12.2477 19.81 12.3047H19.67C19.5662 12.2477 19.466 12.1842 19.37 12.1147C17.6924 10.9866 15.7166 10.3841 13.695 10.3841C11.6734 10.3841 9.6976 10.9866 8.02 12.1147C7.924 12.1842 7.8238 12.2477 7.72 12.3047H7.58C7.4762 12.2477 7.376 12.1842 7.28 12.1147C5.6171 10.9599 3.6343 10.3549 1.61 10.3847H0.41ZM23.62 16.6547C24.236 16.175 24.9995 15.924 25.78 15.9447H27.39V12.7347H25.78C24.4052 12.7181 23.0619 13.146 21.95 13.9547C21.3243 14.416 20.5674 14.6649 19.79 14.6649C19.0126 14.6649 18.2557 14.416 17.63 13.9547C16.4899 13.1611 15.1341 12.7356 13.745 12.7356C12.3559 12.7356 11.0001 13.1611 9.86 13.9547C9.2343 14.416 8.4774 14.6649 7.7 14.6649C6.9226 14.6649 6.1657 14.416 5.54 13.9547C4.4144 13.1356 3.0518 12.7072 1.66 12.7347H0V15.9447H1.61C2.39051 15.924 3.154 16.175 3.77 16.6547C4.908 17.4489 6.2623 17.8747 7.65 17.8747C9.0377 17.8747 10.392 17.4489 11.53 16.6547C12.1468 16.1765 12.9097 15.9257 13.69 15.9447C14.4708 15.9223 15.2348 16.1735 15.85 16.6547C16.9901 17.4484 18.3459 17.8738 19.735 17.8738C21.1241 17.8738 22.4799 17.4484 23.62 16.6547ZM23.62 22.3947C24.236 21.915 24.9995 21.664 25.78 21.6847H27.39V18.4747H25.78C24.4052 18.4581 23.0619 18.886 21.95 19.6947C21.3243 20.156 20.5674 20.4049 19.79 20.4049C19.0126 20.4049 18.2557 20.156 17.63 19.6947C16.4899 18.9011 15.1341 18.4757 13.745 18.4757C12.3559 18.4757 11.0001 18.9011 9.86 19.6947C9.2343 20.156 8.4774 20.4049 7.7 20.4049C6.9226 20.4049 6.1657 20.156 5.54 19.6947C4.4144 18.8757 3.0518 18.4472 1.66 18.4747H0V21.6847H1.61C2.39051 21.664 3.154 21.915 3.77 22.3947C4.908 23.1889 6.2623 23.6147 7.65 23.6147C9.0377 23.6147 10.392 23.1889 11.53 22.3947C12.1468 21.9165 12.9097 21.6657 13.69 21.6847C14.4708 21.6623 15.2348 21.9135 15.85 22.3947C16.9901 23.1884 18.3459 23.6138 19.735 23.6138C21.1241 23.6138 22.4799 23.1884 23.62 22.3947Z"
+              fill="currentColor"
+            />
+          </svg>
+        </a>
+
+        <h2 className="mt-6 text-2xl font-bold text-white sm:text-3xl md:text-4xl">
+          Contact Now
+        </h2>
+
+        <p className="mt-4 leading-relaxed text-white/90">
+          Lorem, ipsum dolor sit amet consectetur adipisicing elit. Eligendi nam dolorum aliquam,
+          quibusdam aperiam voluptatum.
+        </p>
+      </div>
+    </section>
+
+    <main
+      className="flex items-center justify-center px-8 py-8 sm:px-12 lg:col-span-7 lg:px-16 lg:py-12 xl:col-span-6"
+    >
+      <div className="max-w-xl lg:max-w-3xl">
+        <div className="relative -mt-16 block lg:hidden">
+          <a
+            className="inline-flex size-16 items-center justify-center rounded-full bg-white text-blue-600 sm:size-20"
+            href="#"
+          >
+            <span className="sr-only">Home</span>
+            <svg
+              className="h-8 sm:h-10"
+              viewBox="0 0 28 24"
+              fill="none"
+              xmlns="http://www.w3.org/2000/svg"
+            >
+              <path
+                d="M0.41 10.3847C1.14777 7.4194 2.85643 4.7861 5.2639 2.90424C7.6714 1.02234 10.6393 0 13.695 0C16.7507 0 19.7186 1.02234 22.1261 2.90424C24.5336 4.7861 26.2422 7.4194 26.98 10.3847H25.78C23.7557 10.3549 21.7729 10.9599 20.11 12.1147C20.014 12.1842 19.9138 12.2477 19.81 12.3047H19.67C19.5662 12.2477 19.466 12.1842 19.37 12.1147C17.6924 10.9866 15.7166 10.3841 13.695 10.3841C11.6734 10.3841 9.6976 10.9866 8.02 12.1147C7.924 12.1842 7.8238 12.2477 7.72 12.3047H7.58C7.4762 12.2477 7.376 12.1842 7.28 12.1147C5.6171 10.9599 3.6343 10.3549 1.61 10.3847H0.41ZM23.62 16.6547C24.236 16.175 24.9995 15.924 25.78 15.9447H27.39V12.7347H25.78C24.4052 12.7181 23.0619 13.146 21.95 13.9547C21.3243 14.416 20.5674 14.6649 19.79 14.6649C19.0126 14.6649 18.2557 14.416 17.63 13.9547C16.4899 13.1611 15.1341 12.7356 13.745 12.7356C12.3559 12.7356 11.0001 13.1611 9.86 13.9547C9.2343 14.416 8.4774 14.6649 7.7 14.6649C6.9226 14.6649 6.1657 14.416 5.54 13.9547C4.4144 13.1356 3.0518 12.7072 1.66 12.7347H0V15.9447H1.61C2.39051 15.924 3.154 16.175 3.77 16.6547C4.908 17.4489 6.2623 17.8747 7.65 17.8747C9.0377 17.8747 10.392 17.4489 11.53 16.6547C12.1468 16.1765 12.9097 15.9257 13.69 15.9447C14.4708 15.9223 15.2348 16.1735 15.85 16.6547C16.9901 17.4484 18.3459 17.8738 19.735 17.8738C21.1241 17.8738 22.4799 17.4484 23.62 16.6547ZM23.62 22.3947C24.236 21.915 24.9995 21.664 25.78 21.6847H27.39V18.4747H25.78C24.4052 18.4581 23.0619 18.886 21.95 19.6947C21.3243 20.156 20.5674 20.4049 19.79 20.4049C19.0126 20.4049 18.2557 20.156 17.63 19.6947C16.4899 18.9011 15.1341 18.4757 13.745 18.4757C12.3559 18.4757 11.0001 18.9011 9.86 19.6947C9.2343 20.156 8.4774 20.4049 7.7 20.4049C6.9226 20.4049 6.1657 20.156 5.54 19.6947C4.4144 18.8757 3.0518 18.4472 1.66 18.4747H0V21.6847H1.61C2.39051 21.664 3.154 21.915 3.77 22.3947C4.908 23.1889 6.2623 23.6147 7.65 23.6147C9.0377 23.6147 10.392 23.1889 11.53 22.3947C12.1468 21.9165 12.9097 21.6657 13.69 21.6847C14.4708 21.6623 15.2348 21.9135 15.85 22.3947C16.9901 23.1884 18.3459 23.6138 19.735 23.6138C21.1241 23.6138 22.4799 23.1884 23.62 22.3947Z"
+                fill="currentColor"
+              />
+            </svg>
+          </a>
+
+          <h1 className="mt-2 text-2xl font-bold text-gray-900 sm:text-3xl md:text-4xl">
+            Welcome to Squid 🦑
+          </h1>
+
+          <p className="mt-4 leading-relaxed text-gray-500">
+            Lorem, ipsum dolor sit amet consectetur adipisicing elit. Eligendi nam dolorum aliquam,
+            quibusdam aperiam voluptatum.
+          </p>
+        </div>
+
+        <form action="#" className="mt-8 grid grid-cols-6 gap-6">
+          <div className="col-span-6 sm:col-span-3">
+            <label htmlFor="FirstName" className="block text-sm font-medium text-gray-700">
+              First Name
+            </label>
+
+            <input
+              type="text"
+              id="FirstName"
+              name="first_name"
+              className="mt-1 w-full rounded-md border-gray-200 bg-white text-sm text-gray-700 shadow-sm"
+            />
+          </div>
+
+          <div className="col-span-6 sm:col-span-3">
+            <label htmlFor="LastName" className="block text-sm font-medium text-gray-700">
+              Last Name
+            </label>
+
+            <input
+              type="text"
+              id="LastName"
+              name="last_name"
+              className="mt-1 w-full rounded-md border-gray-200 bg-white text-sm text-gray-700 shadow-sm"
+            />
+          </div>
+
+          
+
+          <div className="col-span-6 sm:col-span-3">
+            <label htmlFor="Email" className="block text-sm font-medium text-gray-700"> Email </label>
+
+            <input
+              type="email"
+              id="Email"
+              name="mail"
+              className="mt-1 w-full rounded-md border-gray-200 bg-white text-sm text-gray-700 shadow-sm"
+            />
+          </div>
+
+          <div className="col-span-6 sm:col-span-3">
+            <label htmlFor="Address" className="block text-sm font-medium text-gray-700">
+              Address
+            </label>
+
+            <input
+              type="text"
+              id="Address"
+              name="address"
+              className="mt-1 w-full rounded-md border-gray-200 bg-white text-sm text-gray-700 shadow-sm"
+            />
+          </div>
+          
+          
+          <div className="col-span-6">
+            <label htmlFor="WPnum" className="block text-sm font-medium text-gray-700"> Whatsapp Number </label>
+
+            <input
+              type="text"
+              id="WPnum"
+              name="whatsappNumber"
+              className="mt-1 w-full rounded-md border-gray-200 bg-white text-sm text-gray-700 shadow-sm"
+            />
+          </div>
+
+          <div className="col-span-6">
+            <label htmlFor="Message" className="block text-sm font-medium text-gray-700"> Message </label>
+
+            <input
+              type="text"
+              id="Message"
+              name="text"
+              className="mt-1 w-full rounded-md border-gray-200 bg-white text-sm text-gray-700 shadow-sm"
+            />
+          </div>
+
+          
+
+          <div className="col-span-6">
+            <p className="text-sm text-gray-500">
+              We'll reach out to you ASAP!
+              <a href="#" className="text-gray-700 underline"> terms and conditions </a>
+              and
+              <a href="#" className="text-gray-700 underline">privacy policy</a>.
+            </p>
+          </div>
+
+          <div className="col-span-6 sm:flex sm:items-center sm:gap-4">
+            <button
+              className="inline-block shrink-0 rounded-md border border-blue-600 bg-blue-600 px-12 py-3 text-sm font-medium text-white transition hover:bg-transparent hover:text-blue-600 focus:outline-none focus:ring active:text-blue-500"
+            >
+              Submit
+            </button>
+
+            
+          </div>
+        </form>
+      </div>
+    </main>
+  </div>
+</section>
+     
+  );
+}
+
+
+const FAQSection = () => {
+  return (
+    <section>
+      <div className="px-8 py-24 mx-auto md:px-12 lg:px-32 max-w-7xl">
+        <div>
+          <p className="text-4xl font-semibold tracking-tighter text-gray-900">
+          What Is Yoga Retreat & What Will You gain From It?
+          </p>
+          <p className="mt-4 text-base font-medium text-gray-500">
+            Answers to commonly asked questions about our platform.
+          </p>
+        </div>
+
+        <dl className="grid gap-12 mt-12 lg:grid-cols-2">
+          <div>
+            <dt className="text-lg font-medium text-black">
+              What kind of support can I expect?
+            </dt>
+            <dd className="mt-2 text-base font-medium text-gray-500">
+              We offer comprehensive support including live chat, email, and
+              phone. Our support team is available 24/7 to assist with any
+              issues or questions you might have.
+            </dd>
+          </div>
+          <div>
+            <dt className="text-lg font-medium text-black">
+              How secure is my payment information?
+            </dt>
+            <dd className="mt-2 text-base font-medium text-gray-500">
+              Your payment information is extremely secure. We use
+              industry-standard encryption and comply with PCI standards to
+              ensure your details are protected.
+            </dd>
+          </div>
+          <div>
+            <dt className="text-lg font-medium text-black">
+              Can I cancel my subscription at any time?
+            </dt>
+            <dd className="mt-2 text-base font-medium text-gray-500">
+              Yes, you can cancel your subscription at any time. There are no
+              cancellation fees, though no refunds are provided for partial
+              months.
+            </dd>
+          </div>
+          <div>
+            <dt className="text-lg font-medium text-black">
+              How often are platform updates released?
+            </dt>
+            <dd className="mt-2 text-base font-medium text-gray-500">
+              We regularly update the platform to introduce new features and
+              improvements. Major updates are released quarterly, while minor
+              updates and bug fixes are rolled out as needed.
+            </dd>
+          </div>
+          <div>
+            <dt className="text-lg font-medium text-black">
+              Is there a community or forum where I can discuss with other
+              users?
+            </dt>
+            <dd className="mt-2 text-base font-medium text-gray-500">
+              Yes, we have a community forum where users can share tips, ask
+              questions, and connect with others. It's a great place to learn
+              from fellow users and contribute your own insights.
+            </dd>
+          </div>
+          <div>
+            <dt className="text-lg font-medium text-black">
+              Do you offer training or resources for new users?
+            </dt>
+            <dd className="mt-2 text-base font-medium text-gray-500">
+              Absolutely! We provide a comprehensive knowledge base, video
+              tutorials, and live webinars to help you get started and make the
+              most out of our platform.
+            </dd>
+          </div>
+        </dl>
+      </div>
+    </section>
+  );
+};
 
 
 const TypewriterHeading = () => {
@@ -158,26 +421,24 @@ export const OnePagerSection = () => {
       <div className="px-8 py-24 mx-auto md:px-12 lg:px-32 max-w-7xl">
         <div>
           <h1 className="text-4xl font-semibold tracking-tighter text-gray-900 lg:text-5xl text-balance">
-            Building one pagers together
-            <span className="text-gray-600">wherever and anywhere</span>
+          Subjects/Topics covered in Yoga Retreat
+            <span className="text-gray-600"></span>
           </h1>
           <p className="mt-4 text-base font-medium text-gray-500 text-balance">
-            Control and added security. With decentralization, users have more
-            control over their data and transactions, and the platform is less
-            susceptible to malicious attacks.
+          Topics/Subjects To Be Covered In This Retreat
           </p>
           <div className="grid mt-12 gap-y-12 gap-x-2 md:grid-cols-2">
             <div>
               <div className="p-2 overflow-hidden border rounded-3xl">
                 <img
-                  src="https://plus.unsplash.com/premium_photo-1681488007344-c75b0cf8b0cd?q=80&w=1780&auto=format&fit=crop&ixlib=rb-4.0.3&ixid=M3wxMjA3fDB8MHxwaG90by1wYWdlfHx8fGVufDB8fHx8fA%3D%3D"
+                  src="https://images.ctfassets.net/1tp0n7niw68j/UDd7ttIlStE9HX3w98A3h/d493f8a20f67b67741d5bdaee36241d2/04.jpg"
                   alt="#"
                   className="w-full h-full mx-auto rounded-2xl"
                 />
               </div>
               <div className="mt-4">
                 <p className="font-medium text-gray-900">
-                  No warranty disclaimer
+                Traditional Hatha Yoga (Practical Yoga Classes)
                 </p>
                 <p className="mt-2 text-sm text-gray-500">
                   The license comes with no warranties. The licensor provides
@@ -188,14 +449,14 @@ export const OnePagerSection = () => {
             <div>
               <div className="p-2 overflow-hidden border rounded-3xl">
                 <img
-                  src="https://plus.unsplash.com/premium_photo-1681488007344-c75b0cf8b0cd?q=80&w=1780&auto=format&fit=crop&ixlib=rb-4.0.3&ixid=M3wxMjA3fDB8MHxwaG90by1wYWdlfHx8fGVufDB8fHx8fA%3D%3D"
+                  src="https://miro.medium.com/v2/resize:fit:1400/1*tLnoj0ekQB7foCQiRmM8OQ.jpeg"
                   alt="#"
                   className="w-full h-full mx-auto rounded-2xl"
                 />
               </div>
               <div className="px-8 mt-4">
                 <p className="font-medium text-gray-900">
-                  Modification Freedom
+                Traditional & An Ancient Meditation (Dhyana)
                 </p>
                 <p className="mt-2 text-sm text-gray-500">
                   You can adapt, remix, transform, and build upon the licensed
@@ -206,14 +467,14 @@ export const OnePagerSection = () => {
             <div>
               <div className="p-2 overflow-hidden border rounded-3xl">
                 <img
-                  src="https://plus.unsplash.com/premium_photo-1681488007344-c75b0cf8b0cd?q=80&w=1780&auto=format&fit=crop&ixlib=rb-4.0.3&ixid=M3wxMjA3fDB8MHxwaG90by1wYWdlfHx8fGVufDB8fHx8fA%3D%3D"
+                  src="https://www.yogabasics.com/yogabasics2017/wp-content/uploads/2005/06/chanting-mantras.jpeg"
                   alt="#"
                   className="w-full h-full mx-auto rounded-2xl"
                 />
               </div>
               <div className="px-8 mt-4">
                 <p className="font-medium text-gray-900">
-                  Commercial use allowed
+                Mantra Chanting (Recitation of Sacred Sound)
                 </p>
                 <p className="mt-2 text-sm text-gray-500">
                   You are allowed to use the licensed work for both
@@ -224,14 +485,14 @@ export const OnePagerSection = () => {
             <div>
               <div className="p-2 overflow-hidden border rounded-3xl">
                 <img
-                  src="https://plus.unsplash.com/premium_photo-1681488007344-c75b0cf8b0cd?q=80&w=1780&auto=format&fit=crop&ixlib=rb-4.0.3&ixid=M3wxMjA3fDB8MHxwaG90by1wYWdlfHx8fGVufDB8fHx8fA%3D%3D"
+                  src="https://goqii.com/blog/wp-content/uploads/shutterstock_635284154.jpg"
                   alt="#"
                   className="w-full h-full mx-auto rounded-2xl"
                 />
               </div>
               <div className="px-8 mt-4">
                 <p className="font-medium text-gray-900">
-                  Share alike (SA) absence
+                Pranayama ( Breathing Exercises)
                 </p>
                 <p className="mt-2 text-sm text-gray-500">
                   The CC BY 3.0 License does not include a "Share Alike" (SA)
