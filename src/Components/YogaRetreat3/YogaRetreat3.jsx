b28ffc06ec7d65d8589ac3c9234291447277b7d0
anuragnegi000 <anurag.negi862@gmail.com>
2024-05-29 12:14:33 +0530

    added photo section for retreat

--- src/Components/YogaRetreat3/YogaRetreat3.jsx
+++ src/Components/YogaRetreat3/YogaRetreat3.jsx
@@ -37,13 +37,169 @@ const YogaRetreat3 = () => {
           <LogoHeading heading={"Upcoming Dates for 03 Days Yoga Retreat in Rishikesh"}/>
         
         <PriceGrid/>
+        <LogoHeading heading={"Itinerary of 03 Days Yoga Retreat"}/>
         <div className='p-16'>
         <ScheduleDay1/>
         <ScheduleDay2/>
         </div>
+        <InfiniteIMGAcc/>
+        <InfiniteIMGFood/>
         <Footer/>
     </div>
   ) 
+}
+
+const InfiniteIMGAcc = () => {
+  return (
+    <>
+
+      <div>
+
+        <h1 className='text-4xl m-2 font-bold text-center text-indigo-500 capitalize p-auto'>
+          <TypewriterComponent
+
+            options={{
+              strings: ['Accomodation'],
+              autoStart: true,
+              loop: true,
+              delay: 36,
+              changeDelay: 12,
+
+            }}
+          />
+        </h1>
+      </div>
+      <div class="wrapper">
+        <div class="itemLeft item1">
+          <img src="https://images.unsplash.com/photo-1716369413898-852306c79bcb?q=80&w=1887&auto=format&fit=crop&ixlib=rb-4.0.3&ixid=M3wxMjA3fDB8MHxwaG90by1wYWdlfHx8fGVufDB8fHx8fA%3D%3D" alt="" />
+        </div>
+        <div class="itemLeft item2">
+          <img src="https://images.unsplash.com/photo-1716369413898-852306c79bcb?q=80&w=1887&auto=format&fit=crop&ixlib=rb-4.0.3&ixid=M3wxMjA3fDB8MHxwaG90by1wYWdlfHx8fGVufDB8fHx8fA%3D%3D" alt="" />
+        </div>
+        <div class="itemLeft item3">
+          <img src="https://images.unsplash.com/photo-1716369413898-852306c79bcb?q=80&w=1887&auto=format&fit=crop&ixlib=rb-4.0.3&ixid=M3wxMjA3fDB8MHxwaG90by1wYWdlfHx8fGVufDB8fHx8fA%3D%3D" alt="" />
+        </div>
+        <div class="itemLeft item4">
+          <img src="https://images.unsplash.com/photo-1716369413898-852306c79bcb?q=80&w=1887&auto=format&fit=crop&ixlib=rb-4.0.3&ixid=M3wxMjA3fDB8MHxwaG90by1wYWdlfHx8fGVufDB8fHx8fA%3D%3D" alt="" />
+        </div>
+        <div class="itemLeft item5">
+          <img src="https://images.unsplash.com/photo-1716369413898-852306c79bcb?q=80&w=1887&auto=format&fit=crop&ixlib=rb-4.0.3&ixid=M3wxMjA3fDB8MHxwaG90by1wYWdlfHx8fGVufDB8fHx8fA%3D%3D" alt="" />
+        </div>
+        <div class="itemLeft item6">
+          <img src="https://images.unsplash.com/photo-1716369413898-852306c79bcb?q=80&w=1887&auto=format&fit=crop&ixlib=rb-4.0.3&ixid=M3wxMjA3fDB8MHxwaG90by1wYWdlfHx8fGVufDB8fHx8fA%3D%3D" alt="" />
+        </div>
+        <div class="itemLeft item7">
+          <img src="https://images.unsplash.com/photo-1716369413898-852306c79bcb?q=80&w=1887&auto=format&fit=crop&ixlib=rb-4.0.3&ixid=M3wxMjA3fDB8MHxwaG90by1wYWdlfHx8fGVufDB8fHx8fA%3D%3D" alt="" />
+        </div>
+        <div class="itemLeft item8">
+          <img src="https://images.unsplash.com/photo-1716369413898-852306c79bcb?q=80&w=1887&auto=format&fit=crop&ixlib=rb-4.0.3&ixid=M3wxMjA3fDB8MHxwaG90by1wYWdlfHx8fGVufDB8fHx8fA%3D%3D" alt="" />
+        </div>
+      </div>
+      <div class="wrapper">
+        <div class="itemRight item1">
+          <img src="https://images.unsplash.com/photo-1603361513137-219be38712ed?q=80&w=1776&auto=format&fit=crop&ixlib=rb-4.0.3&ixid=M3wxMjA3fDB8MHxwaG90by1wYWdlfHx8fGVufDB8fHx8fA%3D%3D" alt="" />
+        </div>
+        <div class="itemRight item2">
+          <img src="https://images.unsplash.com/photo-1603361513137-219be38712ed?q=80&w=1776&auto=format&fit=crop&ixlib=rb-4.0.3&ixid=M3wxMjA3fDB8MHxwaG90by1wYWdlfHx8fGVufDB8fHx8fA%3D%3D" alt="" />
+        </div>
+        <div class="itemRight item3">
+          <img src="https://images.unsplash.com/photo-1603361513137-219be38712ed?q=80&w=1776&auto=format&fit=crop&ixlib=rb-4.0.3&ixid=M3wxMjA3fDB8MHxwaG90by1wYWdlfHx8fGVufDB8fHx8fA%3D%3D" alt="" />
+        </div>
+        <div class="itemRight item4">
+          <img src="https://images.unsplash.com/photo-1603361513137-219be38712ed?q=80&w=1776&auto=format&fit=crop&ixlib=rb-4.0.3&ixid=M3wxMjA3fDB8MHxwaG90by1wYWdlfHx8fGVufDB8fHx8fA%3D%3D" alt="" />
+        </div>
+        <div class="itemRight item5">
+          <img src="https://images.unsplash.com/photo-1603361513137-219be38712ed?q=80&w=1776&auto=format&fit=crop&ixlib=rb-4.0.3&ixid=M3wxMjA3fDB8MHxwaG90by1wYWdlfHx8fGVufDB8fHx8fA%3D%3D" alt="" />
+        </div>
+        <div class="itemRight item6">
+          <img src="https://images.unsplash.com/photo-1603361513137-219be38712ed?q=80&w=1776&auto=format&fit=crop&ixlib=rb-4.0.3&ixid=M3wxMjA3fDB8MHxwaG90by1wYWdlfHx8fGVufDB8fHx8fA%3D%3D" alt="" />
+        </div>
+        <div class="itemRight item7">
+          <img src="https://images.unsplash.com/photo-1603361513137-219be38712ed?q=80&w=1776&auto=format&fit=crop&ixlib=rb-4.0.3&ixid=M3wxMjA3fDB8MHxwaG90by1wYWdlfHx8fGVufDB8fHx8fA%3D%3D" alt="" />
+        </div>
+        <div class="itemRight item8">
+          <img src="https://images.unsplash.com/photo-1603361513137-219be38712ed?q=80&w=1776&auto=format&fit=crop&ixlib=rb-4.0.3&ixid=M3wxMjA3fDB8MHxwaG90by1wYWdlfHx8fGVufDB8fHx8fA%3D%3D" alt="" />
+        </div>
+      </div>
+    </>
+  )
+}
+
+
+const InfiniteIMGFood = () => {
+  return (
+    <>
+
+      <div>
+
+        <h1 className='text-4xl m-2 font-bold text-center text-indigo-500 capitalize p-auto'>
+          <TypewriterComponent
+
+            options={{
+              strings: ['Healty and Yogic Food'],
+              autoStart: true,
+              loop: true,
+              delay: 36,
+              changeDelay: 12,
+
+            }}
+          />
+        </h1>
+      </div>
+      <div class="wrapper">
+        <div class="itemLeft item1">
+          <img src="https://images.unsplash.com/photo-1716369413898-852306c79bcb?q=80&w=1887&auto=format&fit=crop&ixlib=rb-4.0.3&ixid=M3wxMjA3fDB8MHxwaG90by1wYWdlfHx8fGVufDB8fHx8fA%3D%3D" alt="" />
+        </div>
+        <div class="itemLeft item2">
+          <img src="https://images.unsplash.com/photo-1716369413898-852306c79bcb?q=80&w=1887&auto=format&fit=crop&ixlib=rb-4.0.3&ixid=M3wxMjA3fDB8MHxwaG90by1wYWdlfHx8fGVufDB8fHx8fA%3D%3D" alt="" />
+        </div>
+        <div class="itemLeft item3">
+          <img src="https://images.unsplash.com/photo-1716369413898-852306c79bcb?q=80&w=1887&auto=format&fit=crop&ixlib=rb-4.0.3&ixid=M3wxMjA3fDB8MHxwaG90by1wYWdlfHx8fGVufDB8fHx8fA%3D%3D" alt="" />
+        </div>
+        <div class="itemLeft item4">
+          <img src="https://images.unsplash.com/photo-1716369413898-852306c79bcb?q=80&w=1887&auto=format&fit=crop&ixlib=rb-4.0.3&ixid=M3wxMjA3fDB8MHxwaG90by1wYWdlfHx8fGVufDB8fHx8fA%3D%3D" alt="" />
+        </div>
+        <div class="itemLeft item5">
+          <img src="https://images.unsplash.com/photo-1716369413898-852306c79bcb?q=80&w=1887&auto=format&fit=crop&ixlib=rb-4.0.3&ixid=M3wxMjA3fDB8MHxwaG90by1wYWdlfHx8fGVufDB8fHx8fA%3D%3D" alt="" />
+        </div>
+        <div class="itemLeft item6">
+          <img src="https://images.unsplash.com/photo-1716369413898-852306c79bcb?q=80&w=1887&auto=format&fit=crop&ixlib=rb-4.0.3&ixid=M3wxMjA3fDB8MHxwaG90by1wYWdlfHx8fGVufDB8fHx8fA%3D%3D" alt="" />
+        </div>
+        <div class="itemLeft item7">
+          <img src="https://images.unsplash.com/photo-1716369413898-852306c79bcb?q=80&w=1887&auto=format&fit=crop&ixlib=rb-4.0.3&ixid=M3wxMjA3fDB8MHxwaG90by1wYWdlfHx8fGVufDB8fHx8fA%3D%3D" alt="" />
+        </div>
+        <div class="itemLeft item8">
+          <img src="https://images.unsplash.com/photo-1716369413898-852306c79bcb?q=80&w=1887&auto=format&fit=crop&ixlib=rb-4.0.3&ixid=M3wxMjA3fDB8MHxwaG90by1wYWdlfHx8fGVufDB8fHx8fA%3D%3D" alt="" />
+        </div>
+      </div>
+      <div class="wrapper">
+        <div class="itemRight item1">
+          <img src="https://images.unsplash.com/photo-1603361513137-219be38712ed?q=80&w=1776&auto=format&fit=crop&ixlib=rb-4.0.3&ixid=M3wxMjA3fDB8MHxwaG90by1wYWdlfHx8fGVufDB8fHx8fA%3D%3D" alt="" />
+        </div>
+        <div class="itemRight item2">
+          <img src="https://images.unsplash.com/photo-1603361513137-219be38712ed?q=80&w=1776&auto=format&fit=crop&ixlib=rb-4.0.3&ixid=M3wxMjA3fDB8MHxwaG90by1wYWdlfHx8fGVufDB8fHx8fA%3D%3D" alt="" />
+        </div>
+        <div class="itemRight item3">
+          <img src="https://images.unsplash.com/photo-1603361513137-219be38712ed?q=80&w=1776&auto=format&fit=crop&ixlib=rb-4.0.3&ixid=M3wxMjA3fDB8MHxwaG90by1wYWdlfHx8fGVufDB8fHx8fA%3D%3D" alt="" />
+        </div>
+        <div class="itemRight item4">
+          <img src="https://images.unsplash.com/photo-1603361513137-219be38712ed?q=80&w=1776&auto=format&fit=crop&ixlib=rb-4.0.3&ixid=M3wxMjA3fDB8MHxwaG90by1wYWdlfHx8fGVufDB8fHx8fA%3D%3D" alt="" />
+        </div>
+        <div class="itemRight item5">
+          <img src="https://images.unsplash.com/photo-1603361513137-219be38712ed?q=80&w=1776&auto=format&fit=crop&ixlib=rb-4.0.3&ixid=M3wxMjA3fDB8MHxwaG90by1wYWdlfHx8fGVufDB8fHx8fA%3D%3D" alt="" />
+        </div>
+        <div class="itemRight item6">
+          <img src="https://images.unsplash.com/photo-1603361513137-219be38712ed?q=80&w=1776&auto=format&fit=crop&ixlib=rb-4.0.3&ixid=M3wxMjA3fDB8MHxwaG90by1wYWdlfHx8fGVufDB8fHx8fA%3D%3D" alt="" />
+        </div>
+        <div class="itemRight item7">
+          <img src="https://images.unsplash.com/photo-1603361513137-219be38712ed?q=80&w=1776&auto=format&fit=crop&ixlib=rb-4.0.3&ixid=M3wxMjA3fDB8MHxwaG90by1wYWdlfHx8fGVufDB8fHx8fA%3D%3D" alt="" />
+        </div>
+        <div class="itemRight item8">
+          <img src="https://images.unsplash.com/photo-1603361513137-219be38712ed?q=80&w=1776&auto=format&fit=crop&ixlib=rb-4.0.3&ixid=M3wxMjA3fDB8MHxwaG90by1wYWdlfHx8fGVufDB8fHx8fA%3D%3D" alt="" />
+        </div>
+      </div>
+    </>
+  )
 }
 
 const RetreatForm = ()=>{
