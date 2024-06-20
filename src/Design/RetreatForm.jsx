530d973632876279f4a2c463b44f0fbe00931752
anuragnegi000 <anurag.negi862@gmail.com>
2024-06-20 11:40:08 +0530

    added loader for retreat form also

--- src/Design/RetreatForm.jsx
+++ src/Design/RetreatForm.jsx
@@ -1,87 +1,59 @@
 import { useState } from "react";
 import emailjs from '@emailjs/browser';
 
-export default function RetreatForm (){
-    const [fullName,setFullName]=useState("");
-    const [email,setEmail]=useState("");
-    const [country,setCountry]=useState("");
-    const [whatsapp,setWhatsapp]=useState("");
-    const [message,setMessage]=useState("");
+export default function RetreatForm() {
+  const [fullName, setFullName] = useState("");
+  const [email, setEmail] = useState("");
+  const [country, setCountry] = useState("");
+  const [whatsapp, setWhatsapp] = useState("");
+  const [message, setMessage] = useState("");
+  const [isLoading, setIsLoading] = useState(false);
 
-    const handleSubmit = (e) => {
-        e.preventDefault();
-    
-        const serviceId = 'service_w3uhl3d';
-        const templateId = 'template_490kqw5';
-        const publicKey = 'VkJWkfkjPxDvdLiRA';
-    
-        const templateParams = {
-          from_name: fullName,
-          to_name:'Mahakaal Yogpeeth',
-          email:email,
-          whatsapp:whatsapp,
-          country:country,
-          message:message
-        };
-    
-          alert("Form Submitted Successfully");
-        emailjs.send(serviceId,templateId,templateParams,publicKey).then((response)=>{
-          console.log("Email has been sent successfully!",response);
-          setFullName('');
-          setEmail('');
-          setWhatsapp('');
-          setCountry('');
-          setMessage('');
-          
-        }).catch((error)=>{
-          console.error('Error sending email: ',error);
-        })
-      };
+  const handleSubmit = (e) => {
+    e.preventDefault();
+    setIsLoading(true);
 
-    return (
-      
-  <section className="bg-white">
-    <div className="lg:grid lg:grid-cols-12">
-      <section className="relative flex h-32 items-end rounded-lg bg-gray-900 lg:col-span-5 lg:h-full xl:col-span-6 ">
-        <img
-          alt=""
-          src="https://img.etimg.com/thumb/msid-77107804,width-640,height-480,imgsize-574047,resizemode-4/pictures-shared.jpg"
-          className="absolute inset-0 h-full w-full object-cover opacity-80"
-        />
-  
-        <div className="hidden lg:relative lg:block lg:p-12">
-          <a className="block text-white" href="#">
-            <span className="sr-only">Home</span>
-            <svg
-              className="h-8 sm:h-10"
-              viewBox="0 0 28 24"
-              fill="none"
-              xmlns="http://www.w3.org/2000/svg"
-            >
-              <path
-                d="M0.41 10.3847C1.14777 7.4194 2.85643 4.7861 5.2639 2.90424C7.6714 1.02234 10.6393 0 13.695 0C16.7507 0 19.7186 1.02234 22.1261 2.90424C24.5336 4.7861 26.2422 7.4194 26.98 10.3847H25.78C23.7557 10.3549 21.7729 10.9599 20.11 12.1147C20.014 12.1842 19.9138 12.2477 19.81 12.3047H19.67C19.5662 12.2477 19.466 12.1842 19.37 12.1147C17.6924 10.9866 15.7166 10.3841 13.695 10.3841C11.6734 10.3841 9.6976 10.9866 8.02 12.1147C7.924 12.1842 7.8238 12.2477 7.72 12.3047H7.58C7.4762 12.2477 7.376 12.1842 7.28 12.1147C5.6171 10.9599 3.6343 10.3549 1.61 10.3847H0.41ZM23.62 16.6547C24.236 16.175 24.9995 15.924 25.78 15.9447H27.39V12.7347H25.78C24.4052 12.7181 23.0619 13.146 21.95 13.9547C21.3243 14.416 20.5674 14.6649 19.79 14.6649C19.0126 14.6649 18.2557 14.416 17.63 13.9547C16.4899 13.1611 15.1341 12.7356 13.745 12.7356C12.3559 12.7356 11.0001 13.1611 9.86 13.9547C9.2343 14.416 8.4774 14.6649 7.7 14.6649C6.9226 14.6649 6.1657 14.416 5.54 13.9547C4.4144 13.1356 3.0518 12.7072 1.66 12.7347H0V15.9447H1.61C2.39051 15.924 3.154 16.175 3.77 16.6547C4.908 17.4489 6.2623 17.8747 7.65 17.8747C9.0377 17.8747 10.392 17.4489 11.53 16.6547C12.1468 16.1765 12.9097 15.9257 13.69 15.9447C14.4708 15.9223 15.2348 16.1735 15.85 16.6547C16.9901 17.4484 18.3459 17.8738 19.735 17.8738C21.1241 17.8738 22.4799 17.4484 23.62 16.6547ZM23.62 22.3947C24.236 21.915 24.9995 21.664 25.78 21.6847H27.39V18.4747H25.78C24.4052 18.4581 23.0619 18.886 21.95 19.6947C21.3243 20.156 20.5674 20.4049 19.79 20.4049C19.0126 20.4049 18.2557 20.156 17.63 19.6947C16.4899 18.9011 15.1341 18.4757 13.745 18.4757C12.3559 18.4757 11.0001 18.9011 9.86 19.6947C9.2343 20.156 8.4774 20.4049 7.7 20.4049C6.9226 20.4049 6.1657 20.156 5.54 19.6947C4.4144 18.8757 3.0518 18.4472 1.66 18.4747H0V21.6847H1.61C2.39051 21.664 3.154 21.915 3.77 22.3947C4.908 23.1889 6.2623 23.6147 7.65 23.6147C9.0377 23.6147 10.392 23.1889 11.53 22.3947C12.1468 21.9165 12.9097 21.6657 13.69 21.6847C14.4708 21.6623 15.2348 21.9135 15.85 22.3947C16.9901 23.1884 18.3459 23.6138 19.735 23.6138C21.1241 23.6138 22.4799 23.1884 23.62 22.3947Z"
-                fill="currentColor"
-              />
-            </svg>
-          </a>
-  
-          <h2 className="mt-6 text-2xl font-bold text-white sm:text-3xl md:text-4xl">
-            Contact Now
-          </h2>
-  
-          
-        </div>
-      </section>
-  
-      <main
-        className="flex items-center justify-center px-8 py-8 sm:px-12 lg:col-span-7 lg:px-16 lg:py-12 xl:col-span-6"
-      >
-        <div className="max-w-xl lg:max-w-3xl">
-          <div className="relative -mt-16 block lg:hidden">
-            <a
-              className="inline-flex size-16 items-center justify-center rounded-full bg-white text-blue-600 sm:size-20"
-              href="#"
-            >
+    const serviceId = 'service_w3uhl3d';
+    const templateId = 'template_490kqw5';
+    const publicKey = 'VkJWkfkjPxDvdLiRA';
+
+    const templateParams = {
+      from_name: fullName,
+      to_name: 'Mahakaal Yogpeeth',
+      email: email,
+      whatsapp: whatsapp,
+      country: country,
+      message: message
+    };
+
+    emailjs.send(serviceId, templateId, templateParams, publicKey)
+      .then((response) => {
+        console.log("Email has been sent successfully!", response);
+        setFullName('');
+        setEmail('');
+        setWhatsapp('');
+        setCountry('');
+        setMessage('');
+        setIsLoading(false);
+        alert("Form Submitted Successfully");
+      })
+      .catch((error) => {
+        console.error('Error sending email: ', error);
+        setIsLoading(false);
+      });
+  };
+
+  return (
+    <section className="bg-white">
+      <div className="lg:grid lg:grid-cols-12">
+        <section className="relative flex h-32 items-end rounded-lg bg-gray-900 lg:col-span-5 lg:h-full xl:col-span-6">
+          <img
+            alt=""
+            src="https://img.etimg.com/thumb/msid-77107804,width-640,height-480,imgsize-574047,resizemode-4/pictures-shared.jpg"
+            className="absolute inset-0 h-full w-full object-cover opacity-80"
+          />
+          <div className="hidden lg:relative lg:block lg:p-12">
+            <a className="block text-white" href="#">
               <span className="sr-only">Home</span>
               <svg
                 className="h-8 sm:h-10"
@@ -95,106 +67,106 @@ export default function RetreatForm (){
                 />
               </svg>
             </a>
-  
-           
+            <h2 className="mt-6 text-2xl font-bold text-white sm:text-3xl md:text-4xl">
+              Contact Now
+            </h2>
           </div>
-  
-          <form onSubmit={handleSubmit} className="mt-8 grid grid-cols-6 gap-6">
-          <div className="col-span-6">
-              <label htmlFor="fullName" className="block text-sm font-medium text-gray-700"> Full Name</label>
-  
+
+          <form onSubmit={handleSubmit} className="space-y-4">
+            <div>
+              <label htmlFor="fullName" className="block text-sm font-medium text-gray-700">Full Name</label>
               <input
                 type="text"
                 id="fullName"
-                name="Full Name"
-                className="mt-1 w-full rounded-md border-gray-200 bg-white text-sm text-gray-700 shadow-sm"
+                className="mt-1 block w-full rounded-md border-gray-300 shadow-sm focus:border-indigo-500 focus:ring-indigo-500 sm:text-sm"
                 value={fullName}
-                onChange={(e)=>setFullName(e.target.value)}
+                onChange={(e) => setFullName(e.target.value)}
+                required
               />
             </div>
-  
-            
-  
-            <div className="col-span-6 sm:col-span-3">
-              <label htmlFor="Email" className="block text-sm font-medium text-gray-700"> Email </label>
-  
+
+            <div>
+              <label htmlFor="email" className="block text-sm font-medium text-gray-700">Email</label>
               <input
                 type="email"
-                id="Email"
-                name="mail"
-                className="mt-1 w-full rounded-md border-gray-200 bg-white text-sm text-gray-700 shadow-sm"
+                id="email"
+                className="mt-1 block w-full rounded-md border-gray-300 shadow-sm focus:border-indigo-500 focus:ring-indigo-500 sm:text-sm"
                 value={email}
-                onChange={(e)=>setEmail(e.target.value)}
+                onChange={(e) => setEmail(e.target.value)}
+                required
               />
             </div>
-  
-            <div className="col-span-6 sm:col-span-3">
-              <label htmlFor="Address" className="block text-sm font-medium text-gray-700">
-                Country
-              </label>
-  
+
+            <div>
+              <label htmlFor="country" className="block text-sm font-medium text-gray-700">Country</label>
               <input
                 type="text"
-                id="Address"
-                name="address"
-                className="mt-1 w-full rounded-md border-gray-200 bg-white text-sm text-gray-700 shadow-sm"
+                id="country"
+                className="mt-1 block w-full rounded-md border-gray-300 shadow-sm focus:border-indigo-500 focus:ring-indigo-500 sm:text-sm"
                 value={country}
-                onChange={(e)=>setCountry(e.target.value)}
-            />
+                onChange={(e) => setCountry(e.target.value)}
+                required
+              />
             </div>
-            
-            
-            <div className="col-span-6">
-              <label htmlFor="WPnum" className="block text-sm font-medium text-gray-700"> Whatsapp Number </label>
-  
+
+            <div>
+              <label htmlFor="whatsapp" className="block text-sm font-medium text-gray-700">Whatsapp Number</label>
               <input
                 type="text"
-                id="WPnum"
-                name="whatsappNumber"
-                className="mt-1 w-full rounded-md border-gray-200 bg-white text-sm text-gray-700 shadow-sm"
+                id="whatsapp"
+                className="mt-1 block w-full rounded-md border-gray-300 shadow-sm focus:border-indigo-500 focus:ring-indigo-500 sm:text-sm"
                 value={whatsapp}
-                onChange={(e)=>setWhatsapp(e.target.value)}
+                onChange={(e) => setWhatsapp(e.target.value)}
+                required
               />
             </div>
-  
-            <div className="col-span-6">
-              <label htmlFor="Message" className="block text-sm font-medium text-gray-700"> Message </label>
-  
-              <input
-                type="text"
-                id="Message"
-                name="text"
-                className="mt-1 w-full rounded-md border-gray-200 bg-white text-sm text-gray-700 shadow-sm"
+
+            <div>
+              <label htmlFor="message" className="block text-sm font-medium text-gray-700">Message</label>
+              <textarea
+                id="message"
+                className="mt-1 block w-full rounded-md border-gray-300 shadow-sm focus:border-indigo-500 focus:ring-indigo-500 sm:text-sm"
                 value={message}
-                onChange={(e)=>setMessage(e.target.value)}
+                onChange={(e) => setMessage(e.target.value)}
+                required
               />
             </div>
-  
-            
-  
-            <div className="col-span-6">
-              <p className="text-sm text-gray-500">
-                We'll reach out to you ASAP!
-                <a href="#" className="text-gray-700 underline"> terms and conditions </a>
-                and
-                <a href="#" className="text-gray-700 underline">privacy policy</a>.
-              </p>
-            </div>
-  
-            <div className="col-span-6 sm:flex sm:items-center sm:gap-4">
+
+            <div>
               <button
-                className="inline-block shrink-0 rounded-md border border-blue-600 bg-blue-600 px-12 py-3 text-sm font-medium text-white transition hover:bg-transparent hover:text-blue-600 focus:outline-none focus:ring active:text-blue-500"
+                type="submit"
+                className="inline-flex w-full items-center justify-center rounded-md border border-transparent bg-indigo-600 px-4 py-2 text-sm font-medium text-white shadow-sm hover:bg-indigo-700 focus:outline-none focus:ring-2 focus:ring-indigo-500 focus:ring-offset-2"
+                disabled={isLoading}
               >
-                Submit
+                {isLoading ? (
+                  <svg
+                    className="h-5 w-5 animate-spin text-white"
+                    xmlns="http://www.w3.org/2000/svg"
+                    fill="none"
+                    viewBox="0 0 24 24"
+                  >
+                    <circle
+                      className="opacity-25"
+                      cx="12"
+                      cy="12"
+                      r="10"
+                      stroke="currentColor"
+                      strokeWidth="4"
+                    ></circle>
+                    <path
+                      className="opacity-75"
+                      fill="currentColor"
+                      d="M4 12a8 8 0 018-8V0C5.373 0 0 5.373 0 12h4zm2 5.291l2.293-2.293-1.414-1.414L4 15.879V18h2v-2.709z"
+                    ></path>
+                  </svg>
+                ) : (
+                  "Submit"
+                )}
               </button>
-  
-              
             </div>
           </form>
-        </div>
-      </main>
-    </div>
-  </section>
-       
-    );
-  }
+        </section>
+      </div>
+    </section>
+  );
+}
